921194f7e05a36386c5c95b04c3b868936d45e6d
Aldrich Wijaya <t111360253@ntut.org.tw>
2022-12-30 21:57:06 +0800

    Finished Deletion Feature

--- History_Function.c
+++ History_Function.c
@@ -3,16 +3,15 @@
 
 void historyIn_Conversion() { //Way to input conversion result to history
     FILE *Pointer;
-    Pointer = fopen("../History_Conversion.txt", "a");
+    Pointer = fopen("../Digital-Logic-Lazy-Caclulate/History_Conversion.txt", "a");
 }
-
 void historyIn_Simplification() { //Way to input simplification result to history
     FILE *Pointer;
 }
 
 void historyDeletionMenu() {
     FILE *Pointer;
-    signed int input;
+    int input;
     char inputYesNo;
     do {
         printf("Input:\n1 to delete conversion history\n2 to delete simplification history\n[Type -1 to end]\n|? - ");
@@ -21,6 +20,7 @@ void historyDeletionMenu() {
             printf("\n");
             return 0;
         }
+        printf("\n");
     } while ((input < 1) || (input > 2)); //Input method with cancel function
 
     switch(input) {
@@ -28,13 +28,29 @@ void historyDeletionMenu() {
         do {
             printf("\nAre you sure you want to delete your conversion history? [y/n]\n|? - ");
             scanf(" %c", &inputYesNo);
-        } while (inputYesNo != 'y' || inputYesNo != 'n');
+        } while ((inputYesNo != 'y') && (inputYesNo != 'Y') && (inputYesNo != 'n') && (inputYesNo != 'N'));
+
+        if ((inputYesNo == 'y') || (inputYesNo == 'Y')) {
+            fclose(fopen("../Digital-Logic-Lazy-Caclulate/History_Conversion.txt", "w"));
+            printf("History deleted!\n");
+        }
+        else
+            break;
 
         break;
 
     case 2:
-        printf("\nAre you sure you want to delete your simplification history? [y/n]\n|? - ");
-        scanf(" %c", &inputYesNo);
+        do {
+            printf("\nAre you sure you want to delete your simplification history? [y/n]\n|? - ");
+            scanf(" %c", &inputYesNo);
+        } while ((inputYesNo != 'y') && (inputYesNo != 'Y') && (inputYesNo != 'n') && (inputYesNo != 'N'));
+
+        if ((inputYesNo == 'y') || (inputYesNo == 'Y')) {
+            fclose(fopen("../Digital-Logic-Lazy-Caclulate/History_Simplification.txt", "w"));
+            printf("History deleted!\n");
+        }
+        else
+            break;
 
         break;
     }
