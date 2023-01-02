77363b3448281c258be1abae132ef2fa7ba25bb3
Aldrich Wijaya <t111360253@ntut.org.tw>
2023-01-02 16:00:13 +0800

    History Display

--- History_Function.c
+++ History_Function.c
@@ -5,8 +5,10 @@ void historyIn_Conversion() { //Way to input conversion result to history
     FILE *Pointer;
     Pointer = fopen("../Digital-Logic-Lazy-Caclulate/History_Conversion.txt", "a");
 }
+
 void historyIn_Simplification() { //Way to input simplification result to history
     FILE *Pointer;
+    Pointer = fopen("../Digital-Logic-Lazy-Caclulate/History_Simplification.txt", "a");
 }
 
 void historyDeletionMenu() {
@@ -56,8 +58,98 @@ void historyDeletionMenu() {
     }
 }
 
+void historyDisplayMenu() {
+    int input;
+
+    do {
+        printf("Input history to be displayed\n1 Conversion History\n2 Simplification History\n[Type -1 to end]\n|? - ");
+        scanf("%d", &input);
+    if (input == -1) {
+            printf("\n");
+            return 0;
+        }
+        printf("\n");
+    } while ((input < 1) || (input > 2)); //Input method with cancel function
+
+    switch (input) {
+    case 1:
+        historyDisplayConversion();
+        break;
+
+    case 2:
+        historyDisplaySimplification();
+        break;
+    }
+}
+
+void historyDisplayConversion() {
+    FILE *Pointer;
+    char character;
+    Pointer = fopen("../Digital-Logic-Lazy-Caclulate/History_Conversion.txt", "r");
+
+    if (Pointer == NULL) { //File missing, end reading
+        printf("Unable to open history file!\n\n");
+        fclose(Pointer);
+        system("pause");
+        return 0;
+    }
+
+    character = fgetc(Pointer);
+    if (character == EOF) { //File is empty, end reading
+        printf("History is empty!\n\n");
+        system("pause");
+        return 0;
+    }
+
+    while (character != EOF) { //Print whole file
+        printf("%c", character);
+        character = fgetc(Pointer);
+    }
+
+    printf("\n");
+    fclose(Pointer);
+
+    system("pause");
+    return 0;
+}
+
+void historyDisplaySimplification () {
+    FILE *Pointer;
+    char character;
+
+    Pointer = fopen("../Digital-Logic-Lazy-Caclulate/History_Simplification.txt", "r");
+
+    if (Pointer == NULL) { //File missing, end reading
+        printf("Unable to open history file!\n\n");
+        fclose(Pointer);
+        system("pause");
+        return 0;
+    }
+
+    character = fgetc(Pointer);
+    if (character == EOF) { //File is empty, end reading
+        printf("History is empty!\n\n");
+        system("pause");
+        return 0;
+    }
+
+    while (character != EOF) { //Print whole file
+        printf("%c", character);
+        character = fgetc(Pointer);
+    }
+
+    printf("\n");
+    fclose(Pointer);
+
+    system("pause");
+    return 0;
+}
+
 int main() {
-    historyDeletionMenu();
+    //historyDeletionMenu();
+    //historyDisplayMenu();
+    //historyDisplayConversion();
+    //historyDisplaySimplification();
 
     system ("pause");
     return 0;
