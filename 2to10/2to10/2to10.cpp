e2e9eee78c2bd92f31b94a06f5e398b862d725b8
zhengxianh86 <zhengxianh86@gmail.com>
2023-01-03 21:24:11 +0800

    V1.7almost there

--- 2to10/2to10/2to10.cpp
+++ 2to10/2to10/2to10.cpp
@@ -2,7 +2,6 @@
 #include<stdlib.h>
 
 const int bin[2] = { 48, 49 };//48=0 49=1
-unsigned long long int Pow(unsigned long long int X, int a);
 unsigned long long int BinToDec(int* B, char* t, int* j);
 int main()
 {
@@ -14,55 +13,41 @@ int main()
 	printf("輸入二進制(至多64位數):");
 	scanf("%s", temp);
 		
-	printf("A%s\n", temp);
-	for (j = 0; j < 64; j++)
-		printf("%d", Bin[j]);
-	printf("\n\n");
-
 	sum = BinToDec(Bin, temp, &i);
-
-	printf("\n\ni= %d B%s\n\n",i, temp);
-
-	for (j = i+1; j < 64; j++)
+	
+	for (j = 64 - i; j < 64; j++)
 		printf("%d", Bin[j]);
-	printf("(%d位數)的十進制為%lld\n\n", 63 - i, sum);
+
+	printf("(%d位數)的十進制為%lld\n\n", i, sum);
 	system("pause");
 	return 0;
 }
 
-unsigned long long int Pow(unsigned long long int X, int a)
+unsigned long long int Pow__(unsigned long long int X, int a)
 {
 	if (a == 0)
 		return 1;
 	else
-		return X * Pow(X, a - 1);
+		return X * Pow__(X, a - 1);
 }
 
 unsigned long long int BinToDec(int* B, char* t, int* i)
 {
-	int j, judge = 0;
-	int regit[64] = { 0 };
+	int j,k;
 	unsigned long long int sum = 0;
 
-	for (j = 0; j < 64; j++)//Bin為01的形式
-		if (*(t + j) == 48)
-			regit[j] = 0;
-		else if(*(t + j) == 49)
-			regit[j] = 1;
+	for (j = 0; *(t + j) != '\0'; j++)
+		*i = j;
+	*i = *i + 1;
 
-	for (j = 63; j < 0; j--)
-		if (judge == 0)
-		{
-			if (*(B + j) == 1)
-			{
-				judge = 1;
-				*(i) = j;
-			}
-		}
-		else if (judge == 0)
-		{
+	for (j = 64 - *i, k = 0; j < 64 || k < 64; j++, k++)//Bin為01的形式
+		if (*(t + k) == 48)
+			*(B + j) = 0;
+		else if (*(t + k) == 49)
+			*(B + j) = 1;
+
+	for (j = 63; j >= 0; j--)
 			if(*(B + j) == 1)
-				sum = sum + Pow(2, 63 - j);
-		}
+				sum = sum + Pow__(2, 63 - j);
 	return sum;
 }
